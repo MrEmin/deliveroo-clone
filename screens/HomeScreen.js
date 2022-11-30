c78c41357f2811130642786bfc2ac3a2c26b887d
MrEmin <emuhammet.ozdemir@gmail.com>
2022-11-30 21:28:26 +0300

    home screen created

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,12 +1,34 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Image } from "react-native";
+import React, { useLayoutEffect } from "react";
+import { useNavigation } from "@react-navigation/native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { ChevronDownIcon } from "react-native-heroicons/outline";
 
 const HomeScreen = () => {
-  return (
-    <View>
-      <Text className="text-red-500">HomeScreen</Text>
-    </View>
-  )
-}
+  const navigation = useNavigation();
 
-export default HomeScreen
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
+  return (
+    <SafeAreaView>
+      <Text className="text-red-500">
+        {/* Header */}
+        <View className="flex-row pb-3 items-center mx-4 space-x-2">
+          <Image source={{ uri: "https://links.papareact.com/wru" }} className="h-7 w-7 bg-gray-300 p-4 rounded-full" />
+        
+          <View>
+            <Text className="font-bold text-gray-400 text-xs">Deliver Now!</Text>
+            <Text className="font-bold text-xl">Current Location
+              <ChevronDownIcon size={20} color="red" />
+            </Text>
+          </View>
+        </View>
+      </Text>
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
