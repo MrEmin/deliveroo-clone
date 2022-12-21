585ce45f58fa598983aa6ee98562776cf51197d7
MrEmin <emuhammet.ozdemir@gmail.com>
2022-12-22 00:03:00 +0300

    body section

---
+++ components/CategoryCard.js
@@ -0,0 +1,13 @@
+import { Text, Image, TouchableOpacity } from "react-native";
+import React from "react";
+
+const CategoryCard = ({ imgUrl, title }) => {
+  return (
+    <TouchableOpacity>
+      <Image source={{ uri: imgUrl }} className="h-20 w-20 rounded" />
+      <Text>{title}</Text>
+    </TouchableOpacity>
+  );
+};
+
+export default CategoryCard;
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,9 +2,15 @@ import { View, Text, Image } from "react-native";
 import React, { useLayoutEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { ChevronDownIcon, UserIcon, MagnifyingGlassIcon ,AdjustmentsVerticalIcon } from "react-native-heroicons/outline";
-import { TextInput } from "react-native-gesture-handler";
+import {
+  ChevronDownIcon,
+  UserIcon,
+  MagnifyingGlassIcon,
+  AdjustmentsVerticalIcon,
+} from "react-native-heroicons/outline";
+import { ScrollView, TextInput } from "react-native-gesture-handler";
 
+import Categories from "../components/Categories";
 
 const HomeScreen = () => {
   const navigation = useNavigation();
@@ -16,32 +22,45 @@ const HomeScreen = () => {
   }, []);
   return (
     <SafeAreaView className="bg-white pt-5">
-      <View className="text-red-500">
-        {/* Header */}
-        <View className="flex-row pb-3 items-center mx-4 space-x-2">
-          <Image source={{ uri: "https://links.papareact.com/wru" }} className="h-7 w-7 bg-gray-300 p-4 rounded-full" />
-        
-          <View className="flex-1">
-            <Text className="font-bold text-gray-400 text-xs">Deliver Now!</Text>
-            <Text className="font-bold text-xl">Current Location
-              <ChevronDownIcon size={20} color="#00CCBB" />
-            </Text>
-          </View>
-          <UserIcon size={35} color="#00CCBB" />
-        </View>
+      {/* Header */}
+      <View className="flex-row pb-3 items-center space-x-2 mx-4">
+        <Image
+          source={{ uri: "https://links.papareact.com/wru" }}
+          className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+        />
 
-        {/* Search */}
-        <View className="flex-row items-center space-x-2 pb-2 mx-4">
-          <View className="flex-row flex-1 space-x-2 bg-gray-200 p-3">
-            <MagnifyingGlassIcon size={20} color="gray" />
-            <TextInput 
-              placeholder="Restaurants and cuisines" 
-              keyboardType="default"
-            />
-          </View>
-          <AdjustmentsVerticalIcon size={30} color="#00CCBB" />
+        <View className="flex-1">
+          <Text className="font-bold text-gray-400 text-xs">Deliver Now!</Text>
+          <Text className="font-bold text-xl">
+            Current Location
+            <ChevronDownIcon size={20} color="#00CCBB" />
+          </Text>
         </View>
+        <UserIcon size={35} color="#00CCBB" />
       </View>
+
+      {/* Search */}
+      <View className="flex-row items-center space-x-2 pb-2 mx-4">
+        <View className="flex-row flex-1 space-x-2 bg-gray-200 p-3">
+          <MagnifyingGlassIcon size={20} color="gray" />
+          <TextInput
+            placeholder="Restaurants and cuisines"
+            keyboardType="default"
+          />
+        </View>
+        <AdjustmentsVerticalIcon size={30} color="#00CCBB" />
+      </View>
+
+      {/* Body */}
+      <ScrollView
+        className="bg-gray-100"
+        contentContainerStyle={{ paddingBottom: 100 }}
+      >
+        {/* Categories */}
+        <Categories />
+
+        {/* Featured Rows */}
+      </ScrollView>
     </SafeAreaView>
   );
 };
