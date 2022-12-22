59e886b24db1755a3927b0fda9ac7d998b3aeb2f
MrEmin <emuhammet.ozdemir@gmail.com>
2022-12-22 15:09:03 +0300

    categories are finished

--- components/Categories.js
+++ components/Categories.js
@@ -14,15 +14,27 @@ const Categories = () => {
     >
       {/* CategoryCard */}
       <CategoryCard
-        imgUrl="https://links.papareact.com/wru"
+        imgUrl="https://links.papareact.com/gn7"
         title="Testing 1"
       />
       <CategoryCard
-        imgUrl="https://links.papareact.com/wru"
+        imgUrl="https://links.papareact.com/gn7"
         title="Testing 2"
       />
       <CategoryCard
-        imgUrl="https://links.papareact.com/wru"
+        imgUrl="https://links.papareact.com/gn7"
+        title="Testing 3"
+      />
+      <CategoryCard
+        imgUrl="https://links.papareact.com/gn7"
+        title="Testing 1"
+      />
+      <CategoryCard
+        imgUrl="https://links.papareact.com/gn7"
+        title="Testing 2"
+      />
+      <CategoryCard
+        imgUrl="https://links.papareact.com/gn7"
         title="Testing 3"
       />
     </ScrollView>
